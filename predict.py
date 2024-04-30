fb308655473391bc0545bc26e558ad52c12df75b
anukul-kiran <balasubramanian120@gmail.com>
2024-04-30 19:59:05 +0530

    Prepare sequence function

--- predict.py
+++ predict.py
@@ -24,3 +24,30 @@ def generate():
     prediction_output = generate_notes(model, network_input, pitchnames, n_vocab)
     create_midi(prediction_output)
 
+def prepare_sequences(notes, pitchnames, n_vocab):
+    ''' prepare the sequences used by the neural network'''
+    # map between notes and integers and back
+    note_to_int = dict((note, number) for number , note in enumerate(pitchnames))
+
+    sequence_length = 100
+    network_input = []
+    output = []
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+
+    # reshape the input into a format compatible with LSTM layers
+    normalized_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
+    # normalize input
+    normalized_input = normalized_input / float(n_vocab)
+
+    return (network_input, normalized_input)
+
+
+
+
+
